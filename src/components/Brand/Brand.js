8aad5e3ce21c7f714186ba7ec3733a4e4643bba3
TalhaSH-10 <128291991+TalhaSH-10@users.noreply.github.com>
2024-05-26 03:34:13 +0500

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SYDNEY</h4> */}
+      {/* <h4>Naseer & Shahzad</h4> */}
       <svg
         width={127}
         height={24}
